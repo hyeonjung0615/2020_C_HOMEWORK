3be06d342f307826c46bbb9e2103f01a8df241c8
hyeonjung0615 <hjn7200@naver.com>
2021-01-16 13:55:25 +0900

    Create hw01_a01.c

---
+++ hw01/hw01_a01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf(" ");
+	printf("      o                                         o                      o o o o o o o o o o o o o      \n ");
+	printf("      o                                         o                 o                o               o  \n ");
+	printf("      o                             o o o o o o o o o o o o o     o              o   o             o  \n ");
+	printf("      o                                   o o o o o o o           o            o       o           o  \n ");
+	printf("      o                                 o               o     o o o          o           o     o o o  \n ");
+	printf("      o                                 o               o         o        o              o        o  \n ");
+	printf("      o o o o o o o o o o o o           o               o         o      o                  o      o  \n ");
+	printf("                                          o o o o o o o       o o o                                o  \n ");
+	printf("                 o                                                o           o o o o o o o o o       \n ");
+	printf("                 o                        o                       o         o                   o     \n ");
+	printf("                 o                        o                       o         o                   o     \n ");
+	printf("                 o                        o                                 o                   o     \n ");
+	printf("     o o o o o o o o o o o o o o          o o o o o o o o o o o o             o o o o o o o o o       \n ");
+	return 0;
+}
